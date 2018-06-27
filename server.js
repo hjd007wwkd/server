1a4cd8120705c831d21aee840df22918f28cf621
Jin <hjd007wwkd@hotmail.com>
2018-06-27 22:08:16 +0000

    update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const IP = process.env.IP || '0.0.0.0';
 var knex = require('knex')({
     client: 'postgresql',
     connection: {
+        connection: 'postgres://kkisdnelugakuz:b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5@ec2-23-23-220-19.compute-1.amazonaws.com:5432/dbl1n6a8vhi4ji',
         user : 'dbl1n6a8vhi4ji',
         password : 'kkisdnelugakuz',
         database : 'b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5'
