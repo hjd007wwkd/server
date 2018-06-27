b951e7ddfa3deeeeceb2fd59dd68efd209125992
Jin <hjd007wwkd@hotmail.com>
2018-06-27 23:37:00 +0000

    update

--- sockets.js
+++ sockets.js
@@ -62,7 +62,8 @@ module.exports = function (server, config, knex) {
             client.join(name);
             client.room = name;
 
-            knex.select().from('messages').then(function(rows) {
+            knex('messages').join('users', 'messages.user_id', 'users.id').join('rooms', 'messages.room_id', 'rooms.id')
+            .select('messages.content', 'messages.created_at', 'users.username').where('rooms.name', name).then(function(rows) {
                 client.emit('message', {type: 'addMessage', payload: rows});
             })
         }
