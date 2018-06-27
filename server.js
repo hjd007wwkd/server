0794e4cf520ac5840d35d5882902e4b43f22c2f6
Jin <hjd007wwkd@hotmail.com>
2018-06-27 22:32:38 +0000

    update

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
 var sockets = require('./sockets');
-var config = require('getconfig')
 const PORT = process.env.PORT || 8888;
 const IP = process.env.IP || '0.0.0.0';
 var knex = require('knex');
+
+var confing = {
+  "isDev": true,
+  "server": {
+    "port": process.env.PORT || 8888,
+    "/* secure */": "/* whether this connects via https */",
+    "secure": false,
+    "key": null,
+    "cert": null,
+    "password": null
+  },
+  "rooms": {
+    "/* maxClients */": "/* maximum number of clients per room. 0 = no limit */",
+    "maxClients": 0
+  },
+  "stunservers": [
+    {
+      "urls": "stun:stun.l.google.com:19302"
+    }
+  ],
+  "turnservers": [
+    {
+      "urls": ["turn:your.turn.servers.here"],
+      "secret": "turnserversharedsecret",
+      "expiry": 86400
+    }
+  ]
+}
 
 app.use(express.static(__dirname+"/public"));
 
