86f4722edc57cf83c13398bef2296b47f4268bc5
Jin <hjd007wwkd@hotmail.com>
2018-06-27 21:47:22 +0000

    update

--- knexfile.js
+++ knexfile.js
@@ -16,9 +16,9 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'chatting',
-      user: 'vagrant',
-      password: '0000'
+      database: 'dbl1n6a8vhi4ji',
+      user: 'kkisdnelugakuz',
+      password: 'b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5'
     },
     migrations: {
       tableName: 'knex_migrations'
