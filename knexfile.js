8567ceff3ff92f04f5be2bdb64a39adfee37b478
Jin <hjd007wwkd@hotmail.com>
2018-06-27 22:52:05 +0000

    update

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,14 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: 'postgres://kkisdnelugakuz:b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5@ec2-23-23-220-19.compute-1.amazonaws.com:5432/dbl1n6a8vhi4ji',
+    connection: {
+      host: 'ec2-23-23-220-19.compute-1.amazonaws.com',
+      database: 'dbl1n6a8vhi4ji',
+      user: 'kkisdnelugakuz',
+      port: '5432',
+      password: 'b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5',
+      url: 'postgres://kkisdnelugakuz:b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5@ec2-23-23-220-19.compute-1.amazonaws.com:5432/dbl1n6a8vhi4ji'
+    },
     migrations: {
       tableName: 'knex_migrations'
     }
