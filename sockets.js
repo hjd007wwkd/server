cee7b54ced969c78be35577d1b5dd0af7cee4088
Jin <hjd007wwkd@hotmail.com>
2018-06-30 05:39:33 +0000

    init

--- sockets.js
+++ sockets.js
@@ -170,14 +170,13 @@ module.exports = function (server, config, knex) {
                 io.in(client.room).emit('message', {type: 'removePeerInfo', peers: clients[client.room]})
             }
 
-            if(activeClients[client.room]) {
-                console.log(client.id);
-                var index = activeClients[client.room].indexOf(client.id);
-                if (index > -1) {
-                  activeClients[client.room].splice(index, 1);
-                }
-                client.to(client.room).emit('message', {type: 'disabled', peers: activeClients[client.room]})
-            }
+            // if(activeClients[client.room]) {
+            //     var index = activeClients[client.room].indexOf(client.id);
+            //     if (index > -1) {
+            //       activeClients[client.room].splice(index, 1);
+            //     }
+            //     client.to(client.room).emit('message', {type: 'disabled', peers: activeClients[client.room]})
+            // }
         }
 
         // we don't want to pass "leave" directly because the
