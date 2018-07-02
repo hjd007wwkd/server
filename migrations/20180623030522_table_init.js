7a3e1cbc4e66dbe3970dea907195137b5cad04f8
Jin <hjd007wwkd@hotmail.com>
2018-07-02 03:24:34 +0000

    init

--- migrations/20180623030522_table_init.js
+++ migrations/20180623030522_table_init.js
@@ -15,14 +15,14 @@ exports.up = function(knex, Promise) {
       table.string('site');
       table.timestamp('date');
       table.specificType('tags', 'text[]');
-      table.string('contenthtml');
-      table.string('contenttext');
+      table.text('contenthtml');
+      table.text('contenttext');
       table.integer('user_id').notNullable();
       table.foreign('user_id').references('id').inTable('users');
     }).then(function() {
   return knex.schema.createTable('messages', function(table) {
       table.increments('id');
-      table.string('content');
+      table.text('content');
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.integer('user_id').notNullable();
       table.foreign('user_id').references('id').inTable('users');
