738ad59ae3580bf55b241f472fcac8bcd7cc46b2
Jin <hjd007wwkd@hotmail.com>
2018-07-02 03:17:21 +0000

    init

--- migrations/20180623030522_table_init.js
+++ migrations/20180623030522_table_init.js
@@ -14,7 +14,7 @@ exports.up = function(knex, Promise) {
       table.string('url');
       table.string('site');
       table.timestamp('date');
-      table.string('tags');
+      table.specificType('tags', 'text[]');
       table.string('contenthtml');
       table.string('contenttext');
       table.integer('user_id').notNullable();
