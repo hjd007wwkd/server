163790030b79b710d2e55ce49fa95c5c7c5a280c
Jin <hjd007wwkd@hotmail.com>
2018-07-08 02:46:53 +0000

    init

--- sockets.js
+++ sockets.js
@@ -188,7 +188,6 @@ module.exports = function (server, config, knex) {
         })
 
         client.on('like', function(username){
-            console.log(username)
             if(clients[client.room][username].userList.includes(client.username)){
                 const index = clients[client.room][username].userList.indexOf(client.username);
                 clients[client.room][username].userList.splice(index, 1);
