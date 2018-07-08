a006b0ae4ef32cc109926571926f88b73309496b
Jin <hjd007wwkd@hotmail.com>
2018-07-08 05:00:01 +0000

    init

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'chatting',
-      user: 'vagrant',
-      password: '0000'
+      database: process.env.DATABASEDEV,
+      user: process.env.USERDEV,
+      password: process.env.PASSWORDDEV
     },
     migrations: {
       tableName: 'knex_migrations'
@@ -16,12 +16,12 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      host: 'ec2-23-23-220-19.compute-1.amazonaws.com',
-      database: 'dbl1n6a8vhi4ji',
-      user: 'kkisdnelugakuz',
-      port: '5432',
-      password: 'b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5',
-      url: 'postgres://kkisdnelugakuz:b2aa7c36f3d3ee6431332ba99ad35260f7399115aad77f4ab66b29a887bb7dd5@ec2-23-23-220-19.compute-1.amazonaws.com:5432/dbl1n6a8vhi4ji'
+      host: process.env.HOSTPRO,
+      database: process.env.DATABASEPRO,
+      user: process.env.USERPRO,
+      port: process.env.PORTPRO,
+      password: process.env.PASSWORDPRO,
+      url: process.env.URLPRO
     },
     migrations: {
       tableName: 'knex_migrations'
